ae0036b25d9879051d76372ceb38d50a22e78248
chullian <noushadchullian@gmail.com>
2023-01-10 10:59:09 +0400

    00

--- lib/ui/screens/projectDetail/bloc/project_detail_state.dart
+++ lib/ui/screens/projectDetail/bloc/project_detail_state.dart
@@ -1,11 +1,11 @@
 part of 'project_detail_bloc.dart';
 
 abstract class ProjectDetailState extends Equatable {
-  ProjectModel projectModel;
-  ProjectDetailState({this.projectModel = ProjectModel()});
+  // ProjectModel projectModel;
+  ProjectDetailState(/* {this.projectModel = ProjectModel()} */);
 
   @override
-  List<Object> get props => [projectModel];
+  List<Object> get props => [/* projectModel */];
 }
 
 class ProjectDetailInitial extends ProjectDetailState {}
